767cfd362b73e500b167b3daa3f647032cd31ceb
metroMediaDev <metromedia.app@proton.me>
2024-09-20 13:40:27 +0200

    Add files via upload

---
+++ test/env.dart
@@ -0,0 +1,373 @@
+// ignore_for_file: prefer_single_quotes, lines_longer_than_80_chars final
+Map<String, dynamic> environment = {
+  "appConfig":
+      "https://metromediadev.github.io/metroMediaApp/delivers/config/config_pl.json",
+  "serverConfig": {"url": "https://metromedia.app", "type": "wordpress"},
+  "defaultDarkTheme": false,
+  "enableRemoteConfigFirebase": false,
+  "enableFirebaseAnalytics": false,
+  "enableFacebookAppEvents": false,
+  "webProxy": "",
+  "maxTextScale": null,
+  "loginSMSConstants": {
+    "nameDefault": "United States",
+    "dialCodeDefault": "+1",
+    "countryCodeDefault": "US"
+  },
+  "phoneNumberConfig": {
+    "enable": false,
+    "selectorType": "BOTTOM_SHEET",
+    "dialCodeDefault": "+1",
+    "showCountryFlag": true,
+    "customCountryList": [],
+    "countryCodeDefault": "US",
+    "useInternationalFormat": true,
+    "selectorFlagAsPrefixIcon": true
+  },
+  "appRatingConfig": {
+    "ios": "1469772800",
+    "android": "com.inspireui.fluxstore",
+    "minDays": 7,
+    "remindDays": 7,
+    "showOnOpen": false,
+    "minLaunches": 10,
+    "remindLaunches": 10
+  },
+  "advanceConfig": {
+    "DefaultLanguage": "pl",
+    "DetailedBlogLayout": "halfSizeImageType",
+    "HideEmptyTags": true,
+    "HideEmptyCategories": true,
+    "hideEmptyRating": true,
+    "ShowBottomCornerCart": true,
+    "GridCount": 3,
+    "isCaching": false,
+    "kIsResizeImage": false,
+    "httpCache": true,
+    "isMultiLanguages": false,
+    "EnableFirebase": false,
+    "RatioProductImage": 1.2,
+    "AlwaysShowTabBar": false,
+    "PrivacyPoliciesPageUrlOrId": "https://inspireui.com/privacy/",
+    "AboutUSPageUrl": "https://codecanyon.net/user/inspireui",
+    "NewsPageUrl": "https://products.inspireui.com/",
+    "FAQPageUrl": "https://products.inspireui.com/have-a-question/",
+    "SupportPageUrl": "https://support.inspireui.com/",
+    "DownloadPageUrl": "https://fluxstore.app/",
+    "SocialConnectUrl": [
+      {
+        "url": "https://www.youtube.com/inspireui?sub_confirmation=1",
+        "icon": "assets/icons/brands/youtube.svg",
+        "name": "Youtube"
+      },
+      {
+        "url": "https://www.facebook.com/inspireUI/",
+        "icon": "assets/icons/brands/facebook.svg",
+        "name": "Facebook"
+      },
+      {
+        "url": "https://twitter.com/InspireUI",
+        "icon": "assets/icons/brands/twitter.svg",
+        "name": "Twitter"
+      }
+    ],
+    "AutoDetectLanguage": false,
+    "MinQueryRadiusDistance": 1,
+    "MaxQueryRadiusDistance": 10,
+    "EnableWooCommerceWholesalePrices": false,
+    "IsRequiredSiteSelection": true,
+    "EnableDeliveryDateOnCheckout": false,
+    "EnableNewSMSLogin": true,
+    "EnableBottomAddToCart": true,
+    "inAppWebView": false,
+    "EnableWOOCSCurrencySwitcher": true,
+    "enableProductBackdrop": false,
+    "categoryImageMenu": true,
+    "categoryImageBoxFit": null,
+    "EnableDigitsMobileLogin": false,
+    "EnableDigitsMobileFirebase": false,
+    "EnableDigitsMobileWhatsApp": false,
+    "WebViewScript": "",
+    "versionCheck": {"enable": false, "iOSAppStoreCountry": "US"},
+    "AjaxSearchURL": "",
+    "AlwaysClearWebViewCache": false,
+    "AlwaysClearWebViewCookie": false,
+    "AlwaysRefreshBlog": false,
+    "OrderNotesLinkSupport": false,
+    "inAppUpdateForAndroid": {"enable": false, "typeUpdate": "flexible"},
+    "categoryConfig": {"deepLevel": 3, "enableLargeCategories": false},
+    "pinnedProductTags": [],
+    "showOpeningStatus": true,
+    "TimeShowToastMessage": 1500,
+    "b2bKingConfig": {
+      "enabled": false,
+      "guestAccessRestriction": "replace_prices_quote"
+    },
+    "EnableTeraWalletWithdrawal": false,
+    "allowGetDatasByCategoryFilter": false,
+    "EnableIsAllData": false,
+    "gdpr": {
+      "confirmCaptcha": "PERMANENTLY DELETE",
+      "showDeleteAccount": true,
+      "showPrivacyPolicyFirstTime": false
+    },
+    "PrivacyPoliciesPageId": null,
+    "showRequestNotification": true
+  },
+  "defaultDrawer": {
+    "logo": "assets/images/logo.png",
+    "items": [
+      {"show": true, "type": "home"},
+      {"show": true, "type": "blog"},
+      {"show": true, "type": "categories"},
+      {"show": true, "type": "cart"},
+      {"show": true, "type": "profile"},
+      {"show": true, "type": "login"},
+      {"show": true, "type": "category"}
+    ],
+    "background": null
+  },
+  "defaultSettings": [
+    "biometrics",
+    "wishlist",
+    "notifications",
+    "language",
+    "darkTheme",
+    "rating",
+    "privacy",
+    "about"
+  ],
+  "loginSetting": {
+    "enable": true,
+    "facebookAppId": "430258564493822",
+    "enableRegister": true,
+    "IsRequiredLogin": false,
+    "appleLoginSetting": {
+      "iOSBundleId": "com.inspireui.mstore.flutter",
+      "appleAccountTeamID": "S9RPAM8224"
+    },
+    "smsLoginAsDefault": false,
+    "isResetPasswordSupported": true,
+    "facebookLoginProtocolScheme": "fb430258564493822",
+    "requireUsernameWhenRegister": false,
+    "showPhoneNumberWhenRegister": false,
+    "requirePhoneNumberWhenRegister": false
+  },
+  "oneSignalKey": {"appID": "", "enable": false},
+  "onBoardingConfig": {
+    "enableOnBoarding": true,
+    "version": 2,
+    "data": [
+      {
+        "title": "Welcome to FluxStore",
+        "desc": "Fluxstore is on the way to serve you. ",
+        "image": "assets/images/fogg-delivery-1.png"
+      },
+      {
+        "title": "Connect Surrounding World",
+        "desc":
+            "See all things happening around you just by a click in your phone. Fast, convenient and clean.",
+        "image": "assets/images/fogg-uploading-1.png"
+      },
+      {
+        "title": "Let's Get Started",
+        "desc": "Waiting no more, let's see what we get!",
+        "image": "assets/images/fogg-order-completed.png"
+      }
+    ],
+    "autoCropImageByDesign": true,
+    "isOnlyShowOnFirstTime": true,
+    "showLanguage": false
+  },
+  "adConfig": {
+    "ads": [
+      {
+        "type": "banner",
+        "iosId": "ca-app-pub-3940256099942544/2934735716",
+        "provider": "google",
+        "androidId": "ca-app-pub-3940256099942544/6300978111",
+        "showOnScreens": ["home", "search"],
+        "waitingTimeToDisplay": 2
+      },
+      {
+        "type": "banner",
+        "iosId": "ca-app-pub-2101182411274198/5418791562",
+        "provider": "google",
+        "androidId": "ca-app-pub-2101182411274198/4052745095"
+      },
+      {
+        "type": "interstitial",
+        "iosId": "ca-app-pub-3940256099942544/4411468910",
+        "provider": "google",
+        "androidId": "ca-app-pub-3940256099942544/4411468910",
+        "showOnScreens": ["profile"],
+        "waitingTimeToDisplay": 5
+      },
+      {
+        "type": "reward",
+        "iosId": "ca-app-pub-3940256099942544/1712485313",
+        "provider": "google",
+        "androidId": "ca-app-pub-3940256099942544/4411468910",
+        "showOnScreens": ["cart"]
+      },
+      {
+        "type": "banner",
+        "iosId": "IMG_16_9_APP_INSTALL#430258564493822_876131259906548",
+        "provider": "facebook",
+        "androidId": "IMG_16_9_APP_INSTALL#430258564493822_489007588618919",
+        "showOnScreens": ["home"]
+      },
+      {
+        "type": "interstitial",
+        "iosId": "430258564493822_489092398610438",
+        "provider": "facebook",
+        "androidId": "IMG_16_9_APP_INSTALL#430258564493822_489092398610438"
+      }
+    ],
+    "enable": false,
+    "adMobAppIdIos": "ca-app-pub-7432665165146018~2664444130",
+    "googleTestingId": [],
+    "adMobAppIdAndroid": "ca-app-pub-7432665165146018~2664444130",
+    "facebookTestingId": ""
+  },
+  "firebaseDynamicLinkConfig": {
+    "link": "https://mstore.io/",
+    "isEnabled": true,
+    "uriPrefix": "https://fluxstoreinspireui.page.link",
+    "iOSBundleId": "com.inspireui.mstore.flutter",
+    "iOSAppStoreId": "1469772800",
+    "androidPackageName": "com.inspireui.fluxstore",
+    "iOSAppMinimumVersion": "1.0.1",
+    "shortDynamicLinkEnable": true,
+    "androidAppMinimumVersion": 1
+  },
+  "dynamicLinkConfig": {
+    "type": "branchIO",
+    "enable": true,
+    "branchIO": {"liveMode": false}
+  },
+  "languagesInfo": [
+    {
+      "code": "pl",
+      "icon": "assets/images/country/pl.png",
+      "name": "Polish",
+      "text": "Język polski",
+      "storeViewCode": "pl"
+    }
+  ],
+  "payTmConfig": {
+    "enabled": true,
+    "merchantId": "your-merchant-id",
+    "production": false,
+    "paymentMethodId": "paytm"
+  },
+  "inAppPurchaseConfig": {
+    "enabled": false,
+    "consumableProductIDs": ["com.inspireui.fluxstore.test"],
+    "subscriptionProductIDs": ["com.inspireui.fluxstore.subscription.test"],
+    "nonConsumableProductIDs": []
+  },
+  "expressPayConfig": {
+    "enabled": true,
+    "merchantId": "merchant.com.inspireui.mstore.flutter",
+    "production": false,
+    "merchantKey": "b2be2ffc-c8b9-11ed-82a9-42eb4e39c8ae",
+    "paymentMethodId": "shahbandrpay",
+    "merchantPassword": "4a00a5fd3c63dd2b743c75746af6ffe2"
+  },
+  "googleApiKey": {
+    "ios": "AIzaSyDSNYVC-8DU9BTcyqkeN9c5pgVhwOBAvGg",
+    "web": "AIzaSyDSNYVC-8DU9BTcyqkeN9c5pgVhwOBAvGg",
+    "android": "AIzaSyDSNYVC-8DU9BTcyqkeN9c5pgVhwOBAvGg"
+  },
+  "productCard": {"defaultImage": "assets/images/no_product_image.png"},
+  "blogDetail": {
+    "showHeart": true,
+    "showComment": true,
+    "showSharing": true,
+    "showAuthorInfo": true,
+    "showRelatedBlog": true,
+    "enableAudioSupport": false,
+    "showTextAdjustment": true
+  },
+  "excludedProductIDs": "",
+  "configChat": {
+    "version": "2",
+    "hideOnScreens": [],
+    "showOnScreens": ["profile"],
+    "EnableSmartChat": true,
+    "enableVendorChat": true,
+    "realtimeChatConfig": {
+      "enable": true,
+      "adminName": "InspireUI",
+      "adminEmail": "admininspireui@gmail.com",
+      "userCanDeleteChat": false,
+      "userCanBlockAnotherUser": false,
+      "adminCanAccessAllChatRooms": false
+    }
+  },
+  "smartChat": [
+    {
+      "app": "firebase",
+      "imageData":
+          "https://trello.com/1/cards/611a38c89ebde41ec7cf10e2/attachments/611a392cceb1b534aa92a83e/previews/611a392dceb1b534aa92a84d/download",
+      "description": "Realtime Chat"
+    },
+    {
+      "app": "chatGPT",
+      "imageData": "https://i.imgur.com/pp1qlPd.png",
+      "description": "Chat GPT"
+    },
+    {
+      "app": "https://wa.me/849908854",
+      "iconData": "whatsapp",
+      "description": "WhatsApp"
+    },
+    {"app": "tel:8499999999", "iconData": "phone", "description": "Call Us"},
+    {"app": "sms://8499999999", "iconData": "sms", "description": "Send SMS"},
+    {
+      "app": "https://tawk.to/chat/5d830419c22bdd393bb69888/default",
+      "iconData": "whatsapp",
+      "description": "Tawk Chat"
+    },
+    {
+      "app": "http://m.me/inspireui",
+      "iconData": "facebookMessenger",
+      "description": "Facebook Chat"
+    },
+    {
+      "app":
+          "https://twitter.com/messages/compose?recipient_id=821597032011931648",
+      "imageData":
+          "https://trello.com/1/cards/611a38c89ebde41ec7cf10e2/attachments/611a38d026894f10dc1091c8/previews/611a38d126894f10dc1091d6/download",
+      "description": "Twitter Chat"
+    }
+  ],
+  "deliveryConfig": {
+    "appLogo": "assets/images/app_icon_transparent.png",
+    "appName": "FluxStore Delivery",
+    "dashboardName1": "FluxStore",
+    "dashboardName2": "Delivery",
+    "enableSystemNotes": false
+  },
+  "managerConfig": {
+    "appLogo": "assets/images/app_icon_transparent.png",
+    "appName": "FluxStore Admin",
+    "enableDeliveryFeature": false
+  },
+  "loadingIcon": {"size": 30, "type": "fadingCube"},
+  "splashScreen": {
+    "type": "flare",
+    "image": "assets/images/splashscreen.flr",
+    "boxFit": "contain",
+    "enable": true,
+    "duration": 2000,
+    "paddingTop": 0,
+    "paddingLeft": 0,
+    "paddingRight": 0,
+    "animationName": "fluxstore",
+    "paddingBottom": 0,
+    "backgroundColor": "#ffffff"
+  },
+  "orderConfig": {"version": 1}
+};
